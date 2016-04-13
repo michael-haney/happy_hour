7fb2f70b95b866e5d90c0f7e332c89118a317892
Michael Haney <michael.haney@colorado.edu>
2016-04-13 12:20:18 -0600

    WIP user_auth

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root 'users#new'
+  resources :users,
+    only: [:new, :create],
+    path_names: { new: 'Signup'}
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
